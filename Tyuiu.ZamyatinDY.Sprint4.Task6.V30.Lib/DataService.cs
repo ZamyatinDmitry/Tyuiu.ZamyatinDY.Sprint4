6eb763db7099cd00db9105a291f307605779210b
ZamyatinDmitry <dimazamatin18@gmail.com>
2023-11-10 03:39:52 +0500

    Task6

---
+++ Tyuiu.ZamyatinDY.Sprint4.Task6.V30.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.ZamyatinDY.Sprint4.Task6.V30.Lib
+{
+    public class DataService : ISprint4Task6V30
+    {
+        public string[] Calculate(string[] array)
+        {
+            var guchi = array.Where(element => element.Length > 5).ToArray();
+            return guchi;
+        }
+
+    }
+}
---
+++ Tyuiu.ZamyatinDY.Sprint4.Task6.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZamyatinDY.Sprint4.Task6.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZamyatinDY.Sprint4.Task6.V30.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("60cee04e-5f93-4ed3-8329-522d1300c149")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
